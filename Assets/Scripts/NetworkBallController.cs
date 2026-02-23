1d0908eb8b788f27254720afc80031398e036dbb
HLivermore108 <hlivermore108@andersonuniversity.edu>
2026-02-22 23:31:29 -0500

    Complete multiplayer Pong with scoring and win conditions
    Tried fixing some more stuff.

--- Assets/Scripts/NetworkBallController.cs
+++ Assets/Scripts/NetworkBallController.cs
@@ -2,18 +2,37 @@ using UnityEngine;
 using Unity.Netcode;
 
 [RequireComponent(typeof(BallMovement))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class NetworkBallController : NetworkBehaviour
 {
     private BallMovement ball;
+    private Rigidbody2D rb;
 
     private void Awake()
     {
         ball = GetComponent<BallMovement>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     public override void OnNetworkSpawn()
     {
-        if (ball != null)
-            ball.enabled = IsServer;
+        if (IsServer)
+        {
+            // Server simulates physics + runs BallMovement
+            if (ball != null) ball.enabled = true;
+            if (rb != null) rb.simulated = true;
+        }
+        else
+        {
+            // Clients do NOT simulate physics (prevents drift then snap-back)
+            if (ball != null) ball.enabled = false;
+
+            if (rb != null)
+            {
+                rb.simulated = false;
+                rb.linearVelocity = Vector2.zero;
+                rb.angularVelocity = 0f;
+            }
+        }
     }
 }
